b851fb578becb0f2e4627514d912c25eec325032
aksoftware98 <aksoftware98@hotmail.com>
2019-12-30 22:37:22 +0200

    Reflection Overview and Entity Framework Code First Engine Example

---
+++ ReflectionDemo.EntityFramework/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ReflectionDemo.EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateSQLTable(typeof(Person), "People");
+            Console.WriteLine("Press any key to close!");
+            Console.ReadKey(); 
+        }
+
+        static void CreateSQLTable(Type type, string tableName)
+        {
+            // Create a connection 
+            var connection = new SqlConnection("Server=.\\SQLExpress;Database=ReflectionDemoDb;Integrated Security=true");
+
+            // Construct the query 
+            // 1- Get the properties 
+            var properties = type.GetProperties();
+
+            var queryBuilder = new StringBuilder();
+            queryBuilder.Append($"CREATE TABLE {tableName} (");
+            var columns = new List<string>();
+            foreach (var property in properties)
+            {
+                // Check if property is primary key 
+                if (property.Name.ToLower() == "id" && property.PropertyType.Name == "Int32") // Auto Increment primary key \
+                    columns.Add("Id int IDENTITY(1,1) PRIMARY KEY");
+
+                if (property.PropertyType.Name == "String")
+                    columns.Add($"{property.Name} nvarchar(max)");
+            }
+            string columnsString = string.Join(',', columns);
+            queryBuilder.Append(columnsString);
+            queryBuilder.Append(")");
+
+            var command = new SqlCommand(queryBuilder.ToString(), connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            Console.WriteLine($"{tableName} has been created successfully!");
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Reflection; 
+
+namespace ReflectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var person = new Person();
+            //Type personType = person.GetType();
+
+            Type personType = typeof(Person);
+
+            var properties = personType.GetProperties();
+            foreach (var item in properties)
+            {
+                Console.WriteLine($"Property: Type: {item.PropertyType.Name} | Name: {item.Name}");
+            }
+
+            Person person = new Person();
+            
+            var methods = personType.GetMethods();
+            foreach (var item in methods)
+            {
+                Console.WriteLine($"Method: Type: {item.ReturnType.Name} | Name: {item.Name}");
+                if(item.Name == "Print")
+                {
+                    item.Invoke(person, null);
+                }
+            }
+            Console.WriteLine("-------------------------");
+            AttributeTest(typeof(Person));
+
+            Console.ReadKey();
+        }
+
+        static void AttributeTest(Type type)
+        {
+            // Get the methods
+            var allMethods = type.GetMethods();
+            var methodsWithAttribute = allMethods.Where(m => m.GetCustomAttribute(typeof(MethodForRunAttribute)) != null);
+
+            var obj = Activator.CreateInstance(type);
+
+            foreach (var item in methodsWithAttribute)
+            {
+                var attribute = (MethodForRunAttribute)item.GetCustomAttribute(typeof(MethodForRunAttribute)); 
+                Console.WriteLine($"{item.Name} run for {attribute.RunCount} times");
+                for (int i = 0; i < attribute.RunCount; i++)
+                {
+                    item.Invoke(obj, null);
+                }
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public int ZipCode { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"{FirstName} {LastName}");
+        }
+
+        [MethodForRun(RunCount = 3)]
+        public void TestMethod()
+        {
+            Console.WriteLine("Hello from Test Method"); 
+        }
+
+        [MethodForRun(RunCount = 1)]
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi {FirstName}");
+        }
+
+        public void Move(int newZipCode)
+        {
+            ZipCode = newZipCode;
+            Console.WriteLine($"{FirstName} {LastName} has been moved to {newZipCode}");
+        }
+    }
+
+    public class MethodForRunAttribute : Attribute
+    {
+        public int RunCount { get; set; }
+    }
+}
